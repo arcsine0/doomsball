3511d898dca31451c759027fa74c4b1f902da557
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2025-02-18 12:30:21 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/electron-vite.animate.svg'
+
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='flex w-screen h-screen'>
+      
+    </div>
+  )
+}
+
+export default App
